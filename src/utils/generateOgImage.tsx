53037f71beb35fd748013476cca21b3e5c82eb3f
Pelle van der Knaap <pelle.van.der.knaap@oberon.nl>
2023-02-26 19:27:41 +0100

    add og images

--- src/utils/generateOgImage.tsx
+++ src/utils/generateOgImage.tsx
@@ -23,12 +23,13 @@ const ogImage = (text: string) => {
   return (
     <div
       style={{
-        background: "#fefbfb",
+        background: "rgb(33, 39, 55)",
         width: "100%",
         height: "100%",
         display: "flex",
         alignItems: "center",
         justifyContent: "center",
+        color: "white",
       }}
     >
       <div
@@ -36,8 +37,8 @@ const ogImage = (text: string) => {
           position: "absolute",
           top: "-1px",
           right: "-1px",
-          border: "4px solid #000",
-          background: "#ecebeb",
+          border: "4px solid rgb(179, 136, 235)",
+          background: "rgb(33, 39, 55)",
           opacity: "0.9",
           borderRadius: "4px",
           display: "flex",
@@ -50,8 +51,8 @@ const ogImage = (text: string) => {
 
       <div
         style={{
-          border: "4px solid #000",
-          background: "#fefbfb",
+          border: "4px solid rgb(179, 136, 235)",
+          background: "rgb(33, 39, 55)",
           borderRadius: "4px",
           display: "flex",
           justifyContent: "center",
