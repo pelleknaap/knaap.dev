f9cd149e7157c61e7dca6120a7718c4cba709375
Pelle van der Knaap <pelle.vd.knaap@xs4all.nl>
2023-11-06 19:19:56 +0100

    fix warnings

--- src/pages/[ogTitle].svg.ts
+++ src/pages/[ogTitle].svg.ts
@@ -2,9 +2,14 @@ import { getCollection } from "astro:content";
 import generateOgImage from "@utils/generateOgImage";
 import type { APIRoute } from "astro";
 
-export const get: APIRoute = async ({ params }) => ({
-  body: await generateOgImage(params.ogTitle),
-});
+export const get: APIRoute = async ({ params }) => {
+  return new Response(
+    await generateOgImage(params.ogTitle), {
+      status: 200,
+      headers: { "Content-Type": "image/svg" },
+    }
+  );
+}
 
 const postImportResult = await getCollection("blog", ({ data }) => !data.draft);
 const posts = Object.values(postImportResult);
--- src/pages/ogs-playground/[ogId].svg.ts
+++ src/pages/ogs-playground/[ogId].svg.ts
@@ -1,11 +1,15 @@
 import type { APIRoute } from "astro";
 import ogMap from "@utils/ogs-playground/map";
+import generateOgImage from "@utils/generateOgImage.tsx";
 
 export const get: APIRoute = async ({ params }) => {
   if (ogMap[params.ogId]) {
-    return {
-      body: await ogMap[params.ogId](),
-    };
+    return new Response(
+      await ogMap[params.ogId](), {
+        status: 200,
+        headers: { "Content-Type": "image/svg" },
+      }
+    );
   }
 };
 
